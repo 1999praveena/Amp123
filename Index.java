4cb86c32be1941a7c092a7d4479b08c19c6fbcd9
Praveena A M <praveenaarokiaraj@gmail.com>
2018-08-27 12:26:54 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Index
+{
+public static void main(String args[])
+{
+int m[]={2,1,3};
+
+System.out.println(m[0]);
+System.pout.println(m[1]);
+System.out.println(m[2]);
+}
+}
